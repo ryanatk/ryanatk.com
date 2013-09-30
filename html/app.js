5b30e9ce43ae7df2e3e362b8d23a625d115f1b78
ryanatk <ryana804@gmail.com>
2013-09-30 00:51:25 -0400

    emptied out to start over

--- html/app.js
+++
@@ -1,8 +0,0 @@
-var http = require('http'),
-    sys = require('sys');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(4040, "127.0.0.1");
-
-sys.log('Server running at http://127.0.0.1:8124/');
